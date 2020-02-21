ebff5fd0681bd2bb459f1a4a30613b8acc54bffe
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2020-02-21 12:32:19 +0100

    Clean up code, switch to jinja2 for templating

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 import pandas as pd
-from html_writer import Html
+from jinja2 import Environment, PackageLoader
 
 
 def print_usage():
@@ -13,15 +13,9 @@ def print_usage():
     quit(-1)
 
 
-def df_to_html_table(df: pd.DataFrame, html: Html):
-    html += df.to_html(render_links=True, classes=['mdl-data-table', 'mdl-js-data-table', 'mdl-shadow--2dp'])
+def df_to_html_table(df: pd.DataFrame):
+    return df.to_html(render_links=True, classes=['mdl-data-table', 'mdl-js-data-table', 'mdl-shadow--2dp'])
 
-
-style = """
-<style>
-   .mdl-data-table {white-space: unset;}
-</style>
-"""
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -33,58 +27,24 @@ if __name__ == "__main__":
     if not os.path.exists(outpath):
         os.mkdir(outpath)
 
-    header = """<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>""" + style
+    env = Environment(
+        loader=PackageLoader('app', 'templates')
+    )
+    template = env.get_template('template.html')
 
     countries = []
     for infile in infiles:
         country = re.sub('\\..+$', '', os.path.basename(infile))
-        countries.append(country)
+        outfile = country + '.html'
+        countries.append({'name': country.capitalize(), 'infile': infile, 'outfile': outfile})
 
-    for infile in infiles:
-        country = re.sub('\\..+$', '', os.path.basename(infile))
-        df = pd.read_csv(infile)
+    for country in countries:
+        df = pd.read_csv(country['infile'])
         df = df.drop(columns=['Description', 'Use', 'Type'])
 
-        html = Html()
-        with html.tag('html'):
-            with html.tag('div', classes=['mdl-layout', 'mdl-js-layout', 'mdl-layout--fixed-drawer']):
-                with html.tag('div', classes=["mdl-layout__drawer"]):
-                    html += '<span class="mdl-layout-title">Countries</span>'
-                    with html.tag('nav', classes=['mdl-navigation']):
-                        for c in countries:
-                            html += '<a class="mdl-navigation__link" href="{}">{}</a>'.format(c + '.html',
-                                                                                              c.capitalize())
-                with html.tag('header', classes=['mdl-layout__header']) as h:
-                    h += header
-                    with html.tag('div', classes=['mdl-layout__header-row']):
-                        with html.tag('span', classes=['mdl-layout-title']) as t:
-                            t += country.capitalize()
-                with html.tag('div', classes=['mdl-grid']):
-                    df_to_html_table(df, html)
+        with open(os.path.join(outpath, country['outfile']), 'w') as file:
+            file.write(template.render(table=df_to_html_table(df), title=country['name'], countries=countries))
 
-        with open(os.path.join(outpath, country + '.html'), 'w') as file:
-            file.write(html.to_raw_html())
-
-    html = Html()
-    with html.tag('html'):
-        html += header
-        with html.tag('div', classes=['mdl-layout', 'mdl-js-layout', 'mdl-layout--fixed-drawer']):
-            with html.tag('div', classes=["mdl-layout__drawer"]):
-                html += '<span class="mdl-layout-title">Countries</span>'
-                with html.tag('nav', classes=['mdl-navigation']):
-                    for country in countries:
-                        html += '<a class="mdl-navigation__link" href="{}">{}</a>'.format(country + '.html',
-                                                                                          country.capitalize())
-            with html.tag('header', classes=['mdl-layout__header']) as h:
-                with html.tag('div', classes=['mdl-layout__header-row']):
-                    with html.tag('span', classes=['mdl-layout-title']) as t:
-                        t += 'Description'
-            with html.tag('main', classes=['mdl-layout__content']):
-                with html.tag('div', classes=['mdl-grid']):
-                    df = pd.read_csv('template.csv')
-                    df_to_html_table(df, html)
-
+    df = pd.read_csv('template.csv')
     with open(os.path.join(outpath, 'index.html'), 'w') as file:
-        file.write(html.to_raw_html())
+        file.write(template.render(table=df_to_html_table(df), title='Description', countries=countries))
